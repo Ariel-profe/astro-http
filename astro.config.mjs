8612a7b18ff2048b98bcf62ec1dfd71e0d788c55
Ariel Elias <eliasariel@live.com>
2025-03-17 12:47:48 -0300

    added cloudfare 5

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,16 +5,11 @@ import sitemap from '@astrojs/sitemap';
 
 import cloudflare from '@astrojs/cloudflare';
 
-// import node from '@astrojs/node';
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   integrations: [mdx(), sitemap()],
 
-  // adapter: node({
-  //   mode: 'standalone',
-  // }),
   output: 'static',
 
   adapter: cloudflare(),
